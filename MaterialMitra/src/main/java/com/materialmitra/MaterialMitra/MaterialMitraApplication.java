c93724bfcfec16bb2c758dfee6f15361d72be216
Rajesh Kumar Gupta <rajeshraj.gupta06@gmail.com>
2025-05-15 00:12:30 +0530

    Add files via upload

---
+++ MaterialMitra/src/main/java/com/materialmitra/MaterialMitra/MaterialMitraApplication.java
@@ -0,0 +1,13 @@
+package com.materialmitra.MaterialMitra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialMitraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaterialMitraApplication.class, args);
+	}
+
+}
---
+++ MaterialMitra/src/test/java/com/materialmitra/MaterialMitra/MaterialMitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.materialmitra.MaterialMitra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaterialMitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
